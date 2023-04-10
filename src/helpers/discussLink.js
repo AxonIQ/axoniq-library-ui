b92d10b673e4398679eee9e589439ce066887b2f
Milen Dyankov <milendyankov@gmail.com>
2023-04-10 15:51:59 +0200

    Update template for creating new discuss post

--- src/helpers/discussLink.js
+++ src/helpers/discussLink.js
@@ -5,11 +5,18 @@ module.exports = (site, page) => {
   var createTitle = encodeURIComponent(`"${page.title}" library page ...`)
   var pageUrl = site.url + page.url
   var createBody = encodeURIComponent(`
+  var createBody = encodeURIComponent(
+`<!-- Replace this comment with your comment/suggestion/question/... -->
+
+
+<!--
+Keep the section below!
+Linking back to the documentation URL not only helps people go to the page
+but also helps find relevant discussions from the documentation page via the "View Discussion" menu.
+-->
 [details="Related library page"]
 [${page.title}](${pageUrl})
 [/details]
-
-Your comment/suggestion/question/...
 `)
 
   var discuss = {}
