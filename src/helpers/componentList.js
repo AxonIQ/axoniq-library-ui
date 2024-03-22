94491c1727df0128d87dd986bdc7d7ae7a1efddf
David Gómez G <dgomezg@gmail.com>
2024-03-22 16:00:55 +0100

    Change in docs structure: Basics, guides and reference

--- src/helpers/componentList.js
+++ src/helpers/componentList.js
@@ -1,10 +1,10 @@
 'use strict'
 
 module.exports = (type, groupsText, components) => {
-  // console.log('Type is: ' + type)
-  // console.log('Groups is: ' + groupsText)
-  // console.log('Components is: ' + JSON.stringify(components))
-  // console.log('---')
+  //console.log('Type is: ' + type)
+  //console.log('Groups is: ' + groupsText)
+  //console.log('Components is: ' + JSON.stringify(components))
+  //console.log('---')
 
   try {
     var groups = JSON.parse(groupsText)
@@ -14,22 +14,25 @@ module.exports = (type, groupsText, components) => {
   }
 
   for (var component in components) {
-    // console.log('Component: ' + component)
+    //console.log('Component: ' + component)
     var componentType = components[component].asciidoc.attributes.type
     if (componentType === type) {
       var componentGroup = components[component].asciidoc.attributes.group
       if (componentGroup === undefined) {
         componentGroup = ''
       }
-      // console.log('Group is: ' + componentGroup)
+      //console.log('Group is: ' + componentGroup)
+      //console.log('groups contains ' + componentGroup + '? ' + Object.hasOwn(groups, componentGroup))
 
-      if (!('components' in groups[componentGroup])) {
-        groups[componentGroup].components = []
+      if (Object.hasOwn(groups, componentGroup)) {
+        if (!('components' in groups[componentGroup])) {
+          groups[componentGroup].components = []
+        }
+        groups[componentGroup].components.push(components[component])
       }
-      groups[componentGroup].components.push(components[component])
     }
   }
 
-  // console.log('result is: ' + JSON.stringify(groups, null, 2))
+  //console.log('result is: ' + JSON.stringify(groups, null, 2))
   return groups
 }
