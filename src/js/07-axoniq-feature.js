1b5dcc755259e9cd1a2520f599cc6e89bff43a62
hatzlj <jakob@jh-software.eu>
2026-04-24 15:58:20 +0200

    Introduce a visual indicator for AxoniqFramework features
    To be able to distinguish Axoniq features in the merged reference-guide, we add a visual indicator that is bound to an antora role (`[.axoniq-feature]``) which translates to a css clas in the rendered doc pages.
    
    The change in article.hbs is required to put the role on root-level headings as well.
    
    relates to https://github.com/AxonIQ/axoniq-framework/issues/60

---
+++ src/js/07-axoniq-feature.js
@@ -0,0 +1,26 @@
+;(function () {
+  'use strict'
+
+  var uiRootPath = (document.getElementById('site-script') || { dataset: {} }).dataset.uiRootPath || '.'
+
+  var headings = document.querySelectorAll(
+    '.axoniq-feature > h1, .axoniq-feature > h2, .axoniq-feature > h3,' +
+      '.axoniq-feature > h4, .axoniq-feature > h5, .axoniq-feature > h6'
+  )
+
+  ;[].slice.call(headings).forEach(function (heading) {
+    var img = document.createElement('img')
+    img.src = uiRootPath + '/img/axoniq-feature.svg'
+    img.alt = 'AxonIQ feature indicator'
+    img.className = 'axoniq-feature-icon'
+
+    var link = document.createElement('a')
+    link.href = 'https://www.axoniq.io/pricing'
+    link.className = 'axoniq-feature-link'
+    link.target = '_blank'
+    link.rel = 'noopener'
+    link.appendChild(img)
+
+    heading.appendChild(link)
+  })
+})()
